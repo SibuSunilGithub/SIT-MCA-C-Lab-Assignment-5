ff2d7c22ca18522a0a7d6146422cc26961eb3642
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-25 11:08:44 +0530

    Add Answer 8
    Compute the area of various geometrical shapes (circle, rectangle, and triangle) using a menu-driven program.

---
+++ q08_display_month_days.c
@@ -0,0 +1,51 @@
+// Read any month number and display it in words along with the number of days for this month.
+
+#include <stdio.h>
+int main()
+{
+    int month_num;
+    printf("Please Enter Month Number (1-12):\n");
+    scanf("%d", &month_num);
+    switch (month_num)
+    {
+    case 1:
+        printf("January - 31");
+        break;
+    case 2:
+        printf("February - 28");
+        break;
+    case 3:
+        printf("March - 31");
+        break;
+    case 4:
+        printf("April - 30");
+        break;
+    case 5:
+        printf("May - 31");
+        break;
+    case 6:
+        printf("June - 30");
+        break;
+    case 7:
+        printf("July - 31");
+        break;
+    case 8:
+        printf("August - 31");
+        break;
+    case 9:
+        printf("September - 30");
+        break;
+    case 10:
+        printf("October - 31");
+        break;
+    case 11:
+        printf("November - 30");
+        break;
+    case 12:
+        printf("December - 31");
+        break;
+    default:
+        printf("Invalid Input!");
+    }
+    return 0;
+}
